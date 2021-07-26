af330b165b1360791a8f7b77650ee1a7fb86cccb
Sumaya Siddiqu <sumaya.siddique@northsouth.edu>
2021-07-27 00:05:10 +0600

    Update main.dart

--- care_4_u_project/lib/main.dart
+++ care_4_u_project/lib/main.dart
@@ -11,33 +11,20 @@ void main() async {
 }
 
 class App extends StatefulWidget {
-  // Create the initialization Future outside of `build`:
   @override
   _AppState createState() => _AppState();
 }
 
 class _AppState extends State<App> {
-  /// The future is part of the state of our widget. We should not call `initializeApp`
-  /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
-
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
-      // Initialize FlutterFire:
       future: _initialization,
       builder: (context, snapshot) {
-        // Check for errors
-        // if (snapshot.hasError) {
-        //   return SomethingWentWrong();
-        // }
-
-        // Once complete, show your application
         if (snapshot.connectionState == ConnectionState.done) {
           return MyApp();
         }
-
-        // Otherwise, show something whilst waiting for initialization to complete
         return CircularProgressIndicator();
       },
     );
