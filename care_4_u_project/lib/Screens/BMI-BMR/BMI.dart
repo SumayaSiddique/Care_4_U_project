de74cdfd8d9b8652cecfbc61cfb2ba83c2b112b7
shafoo98 <67111399+shafoo98@users.noreply.github.com>
2021-09-09 22:57:14 +0600

    BMI fixing

--- care_4_u_project/lib/Screens/BMI-BMR/BMI.dart
+++ care_4_u_project/lib/Screens/BMI-BMR/BMI.dart
@@ -50,128 +50,139 @@ class _BMIState extends State<BMI> {
             this.usermodel = usermodel;
             return Scaffold(
               backgroundColor: Colors.white,
-              appBar: AppBar(
-                backgroundColor: Colors.white,
-                elevation: 0.0,
-                iconTheme: IconThemeData(
-                  color: Colors.black,
-                ),
-              ),
-              body: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text(
-                      'Your BMI Result',
-                      style: TextStyle(
-                        fontSize: Get.textTheme.headline5!.fontSize,
+              body: SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 12.0,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.arrow_back_ios),
+                            onPressed: () {
+                              Get.back();
+                            },
+                          ),
+                        ],
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 200,
-                  ),
-                  Center(
-                    child: Container(
-                      color: Colors.white70,
-                      width: Get.width / 1.5,
-                      height: Get.height / 20.0,
-                      child: Center(
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20.0, vertical: 8.0),
                         child: Text(
-                          bmiValue < 18.5
-                              ? 'You are underweight.'
-                              : bmiValue <= 24.9
-                                  ? 'Your weight is normal.'
-                                  : bmiValue >= 25.0 && bmiValue <= 29.9
-                                      ? 'You are overweight'
-                                      : 'You are obese',
-                          textAlign: TextAlign.center,
+                          'Your BMI Result',
                           style: TextStyle(
-                            fontSize: Get.textTheme.headline6!.fontSize,
-                            color: bmiValue < 18.5
-                                ? Colors.blue
-                                : bmiValue <= 24.9
-                                    ? Colors.green
-                                    : bmiValue >= 25.0 && bmiValue <= 29.9
-                                        ? Colors.yellow
-                                        : Colors.red,
+                            fontSize: Get.textTheme.headline5!.fontSize,
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 12.0,
-                  ),
-                  Center(
-                    child: Container(
-                      width: Get.width / 1.15,
-                      child: SfLinearGauge(
-                        minimum: 13.5,
-                        maximum: 34.5,
-                        showAxisTrack: false,
-                        ranges: <LinearGaugeRange>[
-                          LinearGaugeRange(
-                            endValue: 18.4,
-                            color: Colors.lightBlue,
-                            position: LinearElementPosition.inside,
-                            startWidth: 10.0,
-                            endWidth: 20.0,
-                            edgeStyle: LinearEdgeStyle.startCurve,
-                          ),
-                          LinearGaugeRange(
-                            startValue: 18.5,
-                            endValue: 24.9,
-                            color: Colors.green,
-                            position: LinearElementPosition.inside,
-                            startWidth: 20.0,
-                            endWidth: 20.0,
-                          ),
-                          LinearGaugeRange(
-                            startValue: 25.0,
-                            endValue: 29.9,
-                            color: Colors.yellow[700],
-                            position: LinearElementPosition.inside,
-                            startWidth: 20.0,
-                            endWidth: 20.0,
-                          ),
-                          LinearGaugeRange(
-                            startValue: 30.0,
-                            endValue: 34.9,
-                            color: Colors.red,
-                            position: LinearElementPosition.inside,
-                            startWidth: 20.0,
-                            endWidth: 10.0,
-                            edgeStyle: LinearEdgeStyle.endCurve,
-                          ),
-                        ],
-                        showLabels: false,
-                        markerPointers: [
-                          LinearWidgetPointer(
-                            value: bmiValue,
-                            child: Container(
-                              height: 30,
-                              width: 5,
-                              decoration: BoxDecoration(color: Colors.grey),
+                      SizedBox(
+                        height: 200,
+                      ),
+                      Center(
+                        child: Container(
+                          color: Colors.white70,
+                          width: Get.width / 1.5,
+                          height: Get.height / 20.0,
+                          child: Center(
+                            child: Text(
+                              bmiValue < 18.5
+                                  ? 'You are underweight.'
+                                  : bmiValue <= 24.9
+                                      ? 'Your weight is normal.'
+                                      : bmiValue >= 25.0 && bmiValue <= 29.9
+                                          ? 'You are overweight'
+                                          : 'You are obese',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: Get.textTheme.headline6!.fontSize,
+                                color: bmiValue < 18.5
+                                    ? Colors.blue
+                                    : bmiValue <= 24.9
+                                        ? Colors.green
+                                        : bmiValue >= 25.0 && bmiValue <= 29.9
+                                            ? Colors.yellow
+                                            : Colors.red,
+                              ),
                             ),
                           ),
-                        ],
+                        ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 16.0,
-                  ),
-                  Center(
-                    child: Text(
-                      'Your BMI is ${bmiValue.toPrecision(1)}',
-                      style: TextStyle(
-                        fontSize: Get.textTheme.headline6!.fontSize,
+                      SizedBox(
+                        height: 12.0,
                       ),
-                    ),
+                      Center(
+                        child: Container(
+                          width: Get.width / 1.15,
+                          child: SfLinearGauge(
+                            minimum: 13.5,
+                            maximum: 34.5,
+                            showAxisTrack: false,
+                            ranges: <LinearGaugeRange>[
+                              LinearGaugeRange(
+                                endValue: 18.4,
+                                color: Colors.lightBlue,
+                                position: LinearElementPosition.inside,
+                                startWidth: 10.0,
+                                endWidth: 20.0,
+                                edgeStyle: LinearEdgeStyle.startCurve,
+                              ),
+                              LinearGaugeRange(
+                                startValue: 18.5,
+                                endValue: 24.9,
+                                color: Colors.green,
+                                position: LinearElementPosition.inside,
+                                startWidth: 20.0,
+                                endWidth: 20.0,
+                              ),
+                              LinearGaugeRange(
+                                startValue: 25.0,
+                                endValue: 29.9,
+                                color: Colors.yellow[700],
+                                position: LinearElementPosition.inside,
+                                startWidth: 20.0,
+                                endWidth: 20.0,
+                              ),
+                              LinearGaugeRange(
+                                startValue: 30.0,
+                                endValue: 34.9,
+                                color: Colors.red,
+                                position: LinearElementPosition.inside,
+                                startWidth: 20.0,
+                                endWidth: 10.0,
+                                edgeStyle: LinearEdgeStyle.endCurve,
+                              ),
+                            ],
+                            showLabels: false,
+                            markerPointers: [
+                              LinearWidgetPointer(
+                                value: bmiValue,
+                                child: Container(
+                                  height: 30,
+                                  width: 5,
+                                  decoration: BoxDecoration(color: Colors.grey),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Center(
+                        child: Text(
+                          'Your BMI is ${bmiValue.toPrecision(1)}',
+                          style: TextStyle(
+                            fontSize: Get.textTheme.headline6!.fontSize,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             );
           }
