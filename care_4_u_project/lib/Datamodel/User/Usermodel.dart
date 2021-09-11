f7a6fa9ac432a7effa4fa17c64c29f7940e81531
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-09-12 01:58:21 +0600

    water goal, drankOn and goal added

--- care_4_u_project/lib/Datamodel/User/Usermodel.dart
+++ care_4_u_project/lib/Datamodel/User/Usermodel.dart
@@ -1,8 +1,13 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class Usermodel {
   final String email, password, fullName;
   final int age;
   final double weight, height;
   final bool isMale;
+  final int drank;
+  final int goal;
+  final Timestamp drankOn;
 
   Usermodel({
     required this.fullName,
@@ -12,5 +17,8 @@ class Usermodel {
     required this.height,
     required this.isMale,
     required this.weight,
+    required this.drank,
+    required this.drankOn,
+    required this.goal,
   });
 }
