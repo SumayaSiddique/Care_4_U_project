835143a1828bb76216ff9d476e8d6a91d2430c2e
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-09-12 01:59:28 +0600

    Update UserDetails.dart

--- care_4_u_project/lib/Screens/UserDetails/UserDetails.dart
+++ care_4_u_project/lib/Screens/UserDetails/UserDetails.dart
@@ -71,6 +71,9 @@ class UserDetails extends StatelessWidget {
                         height: snapshot.data!.get('height') / 1.0,
                         weight: snapshot.data!.get('weight') / 1.0,
                         isMale: snapshot.data!.get('isMale'),
+                        goal: snapshot.data!.get('goal'),
+                        drank: snapshot.data!.get('drank'),
+                        drankOn: snapshot.data!.get('drankOn'),
                       );
                       this.usermodel = usermodel;
                       return BuildUserInfo(usermodel: usermodel);
