2126aa330586169d3ccbc3706ead5588b57a1bde
Sumaya Siddique <sumaya.siddique@northsouth.edu>
2021-09-12 01:32:57 +0600

    Corona Status page update

--- care_4_u_project/lib/Screens/CoronaStatus/CoronaStatusPage.dart
+++ care_4_u_project/lib/Screens/CoronaStatus/CoronaStatusPage.dart
@@ -1,8 +1,12 @@
 import 'dart:convert';
 
 import 'package:care_4_u_project/Datamodel/CoronaStatus/CoronaStatusCountryInfoModel.dart';
+import 'package:care_4_u_project/Screens/CoronaStatus/CoronaChart.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
+
+import 'package:get/get.dart';
 
 class CoronaStatusPage extends StatefulWidget {
   const CoronaStatusPage({Key? key}) : super(key: key);
@@ -29,9 +33,19 @@ class _CoronaStatusPageState extends State<CoronaStatusPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Covid-19 Statistics"),
-      ),
+      // appBar: AppBar(
+      //   title: Text("Covid-19 Statistics"),
+      //   actions: [
+      //     // IconButton(
+      //     //   onPressed: () {
+      //     //     Get.to(() => LineChartSample1());
+      //     //   },
+      //     //   icon: Icon(
+      //     //     Icons.ac_unit,
+      //     //   ),
+      //     // ),
+      //   ],
+      // ),
       body: FutureBuilder(
         future: data,
         builder: (BuildContext context, AsyncSnapshot<CoronaStatus> snapshot) {
@@ -66,50 +80,61 @@ class ResultView extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 16.0),
           child: Center(
             child: Text(
-              "Overview",
+              "Corona Statistics",
               style: textTheme.headline4,
             ),
           ),
         ),
-        GridView.count(
-          crossAxisCount: 2,
-          shrinkWrap: true,
-          physics: NeverScrollableScrollPhysics(),
+        Row(
           children: [
-            MediumCard(
-              textTheme: textTheme,
-              containerColor: Colors.blueAccent,
-              number: datamodel.cases!,
-              numberColor: Colors.white,
-              title: 'Cases',
-              titleColor: Colors.white,
+            Flexible(
+              flex: 1,
+              child: CoronaCard(
+                color: Color(0xffFDB258),
+                title: "Affected",
+                number: datamodel.cases.toString(),
+              ),
             ),
-            MediumCard(
-              textTheme: textTheme,
-              containerColor: Colors.red,
-              number: datamodel.deaths!,
-              numberColor: Colors.white,
-              title: 'Deaths',
-              titleColor: Colors.white,
-            ),
-            MediumCard(
-              textTheme: textTheme,
-              containerColor: Colors.green,
-              number: datamodel.recovered!,
-              numberColor: Colors.white,
-              title: 'Recovered',
-              titleColor: Colors.white,
-            ),
-            MediumCard(
-              textTheme: textTheme,
-              containerColor: Colors.blueGrey,
-              number: datamodel.active!,
-              numberColor: Colors.white,
-              title: 'Active',
-              titleColor: Colors.white,
+            Flexible(
+              flex: 1,
+              child: CoronaCard(
+                color: Color(0xffFC5959),
+                title: "Death",
+                number: datamodel.deaths.toString(),
+              ),
             ),
           ],
         ),
+        SizedBox(height: 20),
+        Row(
+          children: [
+            Flexible(
+              flex: 1,
+              child: CoronaCard(
+                color: Color(0xff4AD879),
+                title: "Recovered",
+                number: datamodel.recovered.toString(),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: CoronaCard(
+                color: Color(0xff4BB5FE),
+                title: "Active",
+                number: datamodel.active.toString(),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              child: CoronaCard(
+                color: Color(0xff9058FE),
+                title: "Serious",
+                number: datamodel.critical.toString(),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 30),
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 16.0),
           child: Center(
@@ -163,6 +188,67 @@ class ResultView extends StatelessWidget {
   }
 }
 
+class CoronaCard extends StatelessWidget {
+  const CoronaCard({
+    Key? key,
+    required this.title,
+    required this.number,
+    required this.color,
+  }) : super(key: key);
+  final String title;
+  final String number;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: Container(
+        height: 110,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(12),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 8,
+              color: Colors.black.withOpacity(0.2),
+            )
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 12.0,
+            horizontal: 8,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                title,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 22,
+                ),
+              ),
+              Spacer(),
+              Text(
+                number,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class MediumCard extends StatelessWidget {
   const MediumCard({
     Key? key,
@@ -191,7 +277,7 @@ class MediumCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(16),
         ),
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(12.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
